f4fa4029bfc14d11394d30a6c78c866e76e3b67a
= <=>
2020-04-12 21:09:32 +0700

    add mixi

--- banhang/app/src/main/java/com/example/banhang/activity/MixiDressActivity.java
+++ banhang/app/src/main/java/com/example/banhang/activity/MixiDressActivity.java
@@ -9,15 +9,25 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toolbar;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.banhang.R;
 import com.example.banhang.adapter.MixidressAdapter;
 import com.example.banhang.model.Sanpham;
+import com.example.banhang.ultil.Server;
+import com.example.banhang.ultil.check_connection;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MixiDressActivity extends AppCompatActivity {
 
@@ -28,16 +38,75 @@ public class MixiDressActivity extends AppCompatActivity {
     int idmixidress;
     int page=1;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mixi_dress);
         AnhXa();
-        Getidloaisp();
-        ActionToolBar();
-       // GetData(page);
+        if(check_connection.haveNetworkConnection(getApplicationContext())){
+            Getidloaisp();
+            ActionToolBar();
+            GetData(page);
+        }else{
+            check_connection.ShortToast_Short(getApplicationContext()
+                    ,"Bạn hãy kiểm tra lại kết nối");
+            finish();
+        }
+    }
+
+    private void GetData(int Page) {
+        RequestQueue requestQueue =Volley.newRequestQueue(getApplicationContext());
+        String url ="http://192.168.1.7:8080/Server/getsanpham.php?page="+Page;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                int id =0;
+                String namemaxi ="";
+                int idtypeproduct =0;
+                int pricemaxi=0;
+                String colormaxi ="";
+                String materialmaxi="";
+                String linkmaxi ="";
+                String description="";
+                //int newpro=0;
+                //int incollection=0;
+                if (response !=null){
+                    try {
+                        JSONArray jsonArray = new JSONArray();
+                        for(int i=0;i<jsonArray.length();i++){
+                            JSONObject jsonObject =jsonArray.getJSONObject(i);
+                            id=jsonObject.getInt("idsp");
+                            namemaxi =jsonObject.getString("namesp");
+                            idtypeproduct =jsonObject.getInt("idtype");
+                            pricemaxi = jsonObject.getInt("price");
+                            colormaxi =jsonObject.getString("color");
+                            materialmaxi=jsonObject.getString("material");
+                            linkmaxi =jsonObject.getString("link");
+                            description=jsonObject.getString("desc");
+                            //newpro =jsonObject.getInt("newmaxi");
+                            //incollection =jsonObject.getInt("incollection");
+                            mangmd.add(new Sanpham(id,namemaxi,idtypeproduct,pricemaxi,colormaxi,materialmaxi,linkmaxi,description));
+                            mixidressAdapter.notifyDataSetChanged();
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> param = new HashMap<String, String>();
+                param.put("idtype",String.valueOf(idmixidress));
+                return param;
+            }
+        }; requestQueue.add(stringRequest);
+
     }
 
 //    private void GetData(int Page) {
@@ -66,7 +135,6 @@ public class MixiDressActivity extends AppCompatActivity {
         idmixidress = getIntent().getIntExtra("idloaisanpham",-1);
         Log.d("giatrisanpham",idmixidress+"");
     }
-
     private void AnhXa() {
         toolbarmixidress =(Toolbar) findViewById(R.id.toolbarmixidress);
         lvmd =(ListView) findViewById(R.id.listviewmixidress);
--- banhang/app/src/main/java/com/example/banhang/ultil/Server.java
+++ banhang/app/src/main/java/com/example/banhang/ultil/Server.java
@@ -4,5 +4,5 @@ public class Server {
     public static String localhost="192.168.1.7:8080";
     public static String duongdanloaisp ="http://"+localhost+"/Server/getloaisp.php";
     public static String duongdansanphammoinhat ="http://"+localhost+"/Server/getsanphammoinhat.php";
-    public static String duongdanmixidress ="http://"+localhost+"/Server/getsanpham.php";
+    public static String duongdanmixidress ="http://"+localhost+"/Server/getsanpham.php?page=";
 }
